04d0cb48a0476b55ffed77453c6eef71410a3563
Tero Löppönen <tero.lopponen@gmail.com>
2022-07-25 15:54:22 +0300

    Cleaning code

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import psutil
 import time
 
-# Make GTA V online solo lobby
-for proc in psutil.process_iter(attrs=['pid','name']):
-    if proc.name().lower() == "GTA5.exe".lower():
-        proc.suspend()
-        print("Suspend 8 sec")
-        time.sleep(8)
-        proc.resume()
-        print("Resume")
+def suspend_gtav_process() -> None:
+    """
+    Suspend and resume GTA V online process for solo lobby
+
+    :return: None
+    """
+    for proc in psutil.process_iter(attrs=['pid', 'name']):
+        if proc.name().lower() == "GTA5.exe".lower():
+            proc.suspend()
+            print("Suspend 8 sec")
+            time.sleep(8)
+            proc.resume()
+            print("Resume")
 
 
-
-
+if __name__ == '__main__':
+    suspend_gtav_process()

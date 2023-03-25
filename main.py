4c0d55dbf252f4dd7e269acd1e89dddbfcb8c067
Bugaa <tero.lopponen@gmail.com>
2023-03-25 07:36:05 +0200

    Add support to Red Dead online

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import time
 
 def suspend_gtav_process() -> None:
     """
-    Suspend and resume GTA V online process for solo lobby
+    Suspend and resume GTA V online and RED DEAD ONLINE process for solo lobby
 
     :return: None
     """
     for proc in psutil.process_iter(attrs=['pid', 'name']):
-        if proc.name().lower() == "GTA5.exe".lower():
+        if proc.name().lower() in ["GTA5.exe".lower(), "RDR2.exe".lower()]:
             proc.suspend()
             print("Suspend 8 sec")
             time.sleep(8)
